571487e5f4f4769d4ed69111aa522dbfa23423d9
DevKoukiMaeda <devkoukimaeda@gmail.com>
2022-07-27 11:09:24 +0900

    settings.jsonからタイトル変更

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,14 +2,13 @@ import '../styles/globals.css';
 import '../styles/styles.css';
 import 'tailwindcss/tailwind.css';
 import type { AppProps } from 'next/app';
-
 import Head from 'next/head';
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>デジタルシティサービス</title>
-        <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&display=swap" rel="stylesheet"></link>
+        <title>&#65279;</title>
       </Head>
       <Component {...pageProps} />
     </>
