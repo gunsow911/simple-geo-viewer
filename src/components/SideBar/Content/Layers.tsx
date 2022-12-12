327ad97e79dcac2ced2afd4ed36059a8eea26444
DevKoukiMaeda <DevKoukiMaeda@gmail.com>
2022-12-12 14:59:03 +0900

    fix

--- src/components/SideBar/Content/Layers.tsx
+++ src/components/SideBar/Content/Layers.tsx
@@ -6,7 +6,7 @@ import {
   filterCheckedData,
   getCheckedLayerIdByDataTitleList,
 } from '@/components/LayerFilter/sideBar';
-import { DownloadIcon } from '@/components/SideBar/Icon';
+import { DownloadIcon, smallShareIcon } from '@/components/SideBar/Icon';
 import { useRecoilState } from 'recoil';
 import { LayersState, TemporalLayerConfigState } from '@/store/LayersState';
 import { makeDeckGLLayer } from '@/components/Map/Layer/deckGlLayerFactory';
@@ -171,6 +171,11 @@ export const Layers: FC<LayersProps> = ({ layers }) => {
               {resource.download_url === undefined
                 ? undefined
                 : DownloadIcon(resource.download_url)}
+            </div>
+            <div className="w-1/12">
+              {resource.download_url === undefined
+                ? undefined
+                : smallShareIcon(resource.download_url)}
             </div>
           </div>
         </label>
