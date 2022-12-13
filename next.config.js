4a4b5e57fb8abf3c655116995b27f0d161a8468c
gunsow <fukudatatsuya911@gmail.com>
2022-12-13 09:08:51 +0900

    公開時のサブディレクトリ設定を追加した

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 /* 公開時のサブディレクトリ */
-const SUB_DIRECTORY = '/viewer-private';
+const SUB_DIRECTORY = '/susono-dashboard';
 
 /* 本番環境と開発環境の分岐用のフラグ */
 const isProd = process.env.NODE_ENV == 'production';
