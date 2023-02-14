c88a677a57ad89bd7b7b50ac7adc48cfa08f5730
gunsow <fukudatatsuya911@gmail.com>
2023-02-14 15:07:11 +0900

    テストコードの静的エラーを解消した

--- src/components/Header/index.test.tsx
+++ src/components/Header/index.test.tsx
@@ -1,11 +1,12 @@
 /**
  * @jest-environment jsdom
  */
-import React from 'react';
+import React, { SetStateAction, useState } from 'react';
 import { render } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import Header from '.';
 import { context } from '@/pages';
+import { Disasters, Preferences } from '../LayerFilter/loader';
 
 jest.mock('@/components/Map', () => {
   return function DummyPages(props) {
@@ -27,9 +28,20 @@ describe('Rendering', () => {
   };
   _context.preferences = { settings: mockSettings };
   it('title in json', () => {
+    const disasters: Disasters = {
+      default: 1,
+      data: [],
+    };
+    const [_preference, setPreferrence] = useState<Preferences | null>(null);
+    const [_currentDisaster, setCurrentDisaster] = useState<string>('');
     const header = render(
       <context.Provider value={_context}>
-        <Header />
+        <Header
+          disasters={disasters}
+          isDisaster={true}
+          setPreferrence={setPreferrence}
+          setCurrentDisaster={setCurrentDisaster}
+        />
       </context.Provider>
     );
 
